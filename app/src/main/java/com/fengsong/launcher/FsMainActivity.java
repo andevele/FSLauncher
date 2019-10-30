2af8c8cdb4910a0f0ce6e899a4681eb04b431698
andevele <andevele@163.com>
2019-10-30 17:50:07 +0800

    首次提交

---
+++ app/src/main/java/com/fengsong/launcher/FsMainActivity.java
@@ -0,0 +1,13 @@
+package com.fengsong.launcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FsMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
