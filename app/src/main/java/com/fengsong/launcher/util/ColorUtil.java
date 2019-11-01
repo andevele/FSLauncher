ede86b1c31076919079ca9bd0d3259265e64a0b7
andevele <andevele@163.com>
2019-11-01 14:54:26 +0800

    补充工具类

---
+++ app/src/main/java/com/fengsong/launcher/util/ColorUtil.java
@@ -0,0 +1,35 @@
+package com.fengsong.launcher.util;
+
+import java.util.Random;
+
+/**
+ * zhulf 20191031
+ * andevele@163.com
+ * 随机颜色值
+ */
+public class ColorUtil {
+    static String[] colors = {
+            "#141414", //黑
+            "#ffffff", //白
+            "#D2691E",
+            "#555555",
+            "#8B0000",
+            "#008B8B",
+            "#6495ED",
+            "#3CB371",
+            "#4169E1",
+            "#8B4513",
+            "#228B22",
+            "#808000"
+    };
+
+    public static String getRandomColor() {
+        Random random = new Random();
+        int i = random.nextInt(colors.length);
+        return colors[i];
+    }
+
+    public static String getColors(int index) {
+        return colors[index];
+    }
+}
---
+++ app/src/main/java/com/fengsong/launcher/util/Constant.java
@@ -0,0 +1,43 @@
+package com.fengsong.launcher.util;
+
+/**
+ * zhulf 20191031
+ * andevele@163.com
+ * 常量
+ */
+public class Constant {
+    public static final int YOUTUBE_INDEX = 0;
+    public static final int NETFLIX_INDEX = 1;
+    public static final int HOTSTAR_INDEX = 2;
+    public static final int WIRELESS_DISPLAY_INDEX = 3;
+    public static final int PRIME_VIDEO_INDEX = 4;
+    public static final int APPS_INDEX = 5;
+    public static final int TV_STORE_INDEX = 6;
+
+    public static final String INPUT_SOURCE_NAME_ATV = "ATV";
+    public static final String INPUT_SOURCE_NAME_DTV = "DTV";
+    public static final String INPUT_SOURCE_NAME_AV1 = "AV1";
+    public static final String INPUT_SOURCE_NAME_AV2 = "AV2";
+    public static final String INPUT_SOURCE_NAME_HDMI1 = "HDMI1";
+    public static final String INPUT_SOURCE_NAME_HDMI2 = "HDMI2";
+    public static final String INPUT_SOURCE_NAME_YPBPR1 = "YPBPR1";
+    public static final String INPUT_SOURCE_NAME_YPBPR2 = "YPBPR2";
+    public static final String INPUT_SOURCE_NAME_VGA = "VGA";
+    public static final String INPUT_SOURCE_NAME_MEDIA = "MEDIA";
+
+    public static final String INPUT_SOURCE_JSON_FILE = "/tvcustomer/Customer/source.json";
+    public static final String INPUT_SOURCE_SECTION = "inputsource";
+    public static final String ACTION_USB_MOUNTED = "action.usb.mounted";
+
+    public static final String APPS_NAME_APTOIDE = "Aptoide TV";
+    public static final String APPS_NAME_HOTSTAR = "Hotstar";
+    public static final String APPS_NAME_YOUTUBE = "YouTube";
+    public static final String APPS_NAME_PRIMEVIDEO = "Prime Video";
+    public static final String APPS_NAME_LOCALMM = "LocalMM";
+    public static final String APPS_NAME_DOWNLOAD = "Downloads";
+    public static final String APPS_NAME_APK_MANAGER = "APK Manager";
+    public static final String APPS_NAME_FILE_EXPLORER = "File Explorer";
+    public static final String APPS_NAME_FILE_SEARCH = "Search";
+    public static final String APPS_NAME_FILE_MCAST = "Mcast";
+
+}
